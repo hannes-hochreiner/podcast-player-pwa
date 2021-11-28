6947855f900a0d58a03d3108cd9d5c480509e34c
Hannes Hochreiner <hannes@hochreiner.net>
2021-11-28 21:17:17 +0100

    add player to top

--- src/components/top.rs
+++ src/components/top.rs
@@ -1,12 +1,14 @@
 use super::router::Router;
-use crate::agents::updater;
+use crate::agents::{player, updater};
 use yew::{prelude::*, Component};
 
 pub struct Top {
     _updater: Box<dyn Bridge<updater::Updater>>,
+    _player: Box<dyn Bridge<player::Player>>,
 }
 pub enum Message {
     UpdaterMessage(updater::Response),
+    PlayerMessage(player::Response),
 }
 
 impl Component for Top {
@@ -14,16 +16,19 @@ impl Component for Top {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: yew::ComponentLink<Self>) -> Self {
-        let cb = link.callback(Message::UpdaterMessage);
+        let updater_cb = link.callback(Message::UpdaterMessage);
+        let player_cb = link.callback(Message::PlayerMessage);
 
         Self {
-            _updater: updater::Updater::bridge(cb),
+            _updater: updater::Updater::bridge(updater_cb),
+            _player: player::Player::bridge(player_cb),
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> yew::ShouldRender {
         match msg {
             Message::UpdaterMessage(_resp) => false,
+            Message::PlayerMessage(_resp) => false,
         }
     }
 
