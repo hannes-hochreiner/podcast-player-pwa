c83e2ecd444338b503d570cb9062846fb6bdb3bf
Hannes Hochreiner <hannes@hochreiner.net>
2022-10-24 20:07:08 +0200

    remove unneeded file

--- src/objects/auth0_token.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Auth0Token {
-    pub access_token: String,
-    pub expires_in: i64,
-    pub token_type: String,
-}
